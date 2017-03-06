9785ca67d79a3ffd06fd590b21ef8ecb99a64a80
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2017-03-06 20:25:45 +0000

    Another bug…

--- ca.ubc.stat.blang.parent/ca.ubc.stat.blang/src/ca/ubc/stat/blang/StaticJavaUtils.java
+++ ca.ubc.stat.blang.parent/ca.ubc.stat.blang/src/ca/ubc/stat/blang/StaticJavaUtils.java
@@ -1,28 +1,25 @@
 package ca.ubc.stat.blang;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import blang.core.ModelBuilder;
 
 public class StaticJavaUtils
 {
-  public static void callRunner(Class<? extends ModelBuilder> modelBuilderClass, String [] args) throws 
-      ClassNotFoundException, 
-      NoSuchMethodException, 
-      SecurityException, 
-      IllegalAccessException, 
-      IllegalArgumentException, 
-      InvocationTargetException {
-    Class<?> runnerClass = Class.forName("blang.runtime.Runner");
-    Method mainMethod = runnerClass.getMethod("main", String[].class);
-    String [] modifiedArgs = new String[args.length + 2];
-    for (int i = 0; i < args.length; i++) {
-      modifiedArgs[i] = args[i];
+  public static void callRunner(Class<? extends ModelBuilder> modelBuilderClass, String [] args) {
+    try {
+      Class<?> runnerClass = Class.forName("blang.runtime.Runner");
+      Method mainMethod = runnerClass.getMethod("main", String[].class);
+      Object [] modifiedArgs = new Object[args.length + 2];
+      for (int i = 0; i < args.length; i++) {
+        modifiedArgs[i] = args[i];
+      }
+      modifiedArgs[args.length] = "--model";
+      modifiedArgs[args.length + 1] = modelBuilderClass.getName();
+      mainMethod.invoke(null, modifiedArgs);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
-    modifiedArgs[args.length] = "--model";
-    modifiedArgs[args.length + 1] = modelBuilderClass.getName();
-    mainMethod.invoke(null, modifiedArgs);
   }
   
   public static Class<?> STRING_ARRAY = String[].class;
